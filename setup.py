9b02ce7c3af9eb3fe8265c284d462e23d5223243
Pugno <daniele.pugno@scidom.de>
2022-03-28 17:14:19 +0200

    change in setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lassonet_fab',
+    name='lassonetfab',
     packages=find_packages(),
     url='https://github.com/gnopuz83/lassonet',
     description='This is a lassonet implementation',
@@ -9,6 +9,6 @@ setup(
     dependency_links = [
      "git+git@github.com:gnopuz83/lassonet.git",
     ],
-    package = ["lassonet_fab"],
+    package = ["lassonetfab"],
     include_package_data=True,
 )
